694e56c7faefb5697d73956e833509ff256ce9e9
Maria Eduarda Justino <madujustinostudies@gmail.com>
2025-01-18 21:58:53 -0300

    Update Board.jsx

--- to-do list/src/components/Board.jsx
+++ to-do list/src/components/Board.jsx
@@ -18,13 +18,21 @@ const Board = ({ task, index, taskList, setTaskList }) => {
       className={`w-1/2 max-w-xl flex items-center justify-between border rounded-xl px-4 py-1 text-lg md:px-6 ${
         isCompleted ? "line-through text-gray-500" : ""
       }`}
+      style={{
+        backgroundColor: 'rgba(255, 105, 180, 0.5)',
+        border: '1px solid white',
+      }}
     >
       <p onClick={toggleComplete} className="cursor-pointer flex-grow text-left">
         {task}
       </p>
       <button
         onClick={handleDelete}
-        className="bg-red-400 hover:bg-red-500 text-white rounded-lg py-1 px-2 ml-4"
+        style={{
+          backgroundColor: 'rgba(255, 105, 180, 0.8)',
+          border: '1px solid white',
+        }}
+        className="text-white rounded-lg py-1 px-2 ml-4"
       >
         x
       </button>
